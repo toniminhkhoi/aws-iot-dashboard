04da70c458d47fc77a3cd2328162eb1b889a7b87
toniminhkhoi <khoi.pham2005ktmt@hcmut.edu.vn>
2026-07-28 17:04:32 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://18.141.166.190:8000', // <-- Trỏ về Backend Local
+        target: 'http://47.129.13.52:8000', // <-- Trỏ về Backend Local
         changeOrigin: true,
         secure: false,
       },
